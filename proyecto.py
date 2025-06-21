bbafd0d0314684804602a24803ad68860b70b7a6
mariotiradom <mapajatima0120@gmail.com>
2025-06-20 20:42:03 -0500

    archivos iniciales

---
+++ proyecto.py
@@ -0,0 +1,36 @@
+# cargar librerías
+import pandas as pd
+
+# definir funciones
+def load_data(file_path):
+    try:
+        data = pd.read_excel(file_path, header = 3)
+        return data
+    except Exception as e:
+        print(f"Error al cargar la data de {file_path}: {e}")
+        return None
+
+# copiar ruta de los archivos
+siniestros_path = 'C:/Users/User_001/Documents/PACD/proyecto/BBDD ONSV - SINIESTROS 2021-2023.xlsx' # modificar según sea necesario
+vehiculos_path = 'C:/Users/User_001/Documents/PACD/proyecto/BBDD ONSV - VEHICULOS 2021-2023.xlsx' # modificar según sea necesario
+
+# cargar datos
+siniestros = load_data(siniestros_path)
+vehiculos = load_data(vehiculos_path)
+
+# limpiar siniestros 'CÓDIGO SINIESTRO'
+siniestros = siniestros.drop_duplicates(subset = 'CÓDIGO SINIESTRO', keep = 'first')
+print(siniestros.shape)
+print(vehiculos.columns)
+
+# seleccionar columnas de interés
+siniestros = siniestros[['CÓDIGO SINIESTRO', 'FECHA SINIESTRO', 'HORA SINIESTRO', 'CLASE SINIESTRO', 'DEPARTAMENTO', 'PROVINCIA', 'DISTRITO', 'TIPO DE VÍA', 'COORDENADAS LATITUD', 'COORDENADAS  LONGITUD', 'CONDICIÓN CLIMÁTICA', 'SUPERFICIE DE CALZADA', 'CAUSA FACTOR PRINCIPAL']]
+vehiculos = vehiculos[['CÓDIGO SINIESTRO', 'VEHÍCULO', 'MES', 'DÍA', 'HORA', 'AÑO']]
+
+# realizar merge entre siniestros y vehículos
+vehiculos_siniestros = pd.merge(siniestros, vehiculos, on = 'CÓDIGO SINIESTRO', how = 'inner')
+print(vehiculos_siniestros.shape)
+
+# mostrar las primeras filas del dataframe resultante
+print(vehiculos_siniestros.head(10))
+
